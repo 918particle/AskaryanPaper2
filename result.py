813e68abf25dc64c955b727d47bdc05ae79dba85
Jordan C hanson <918particle@gmail.com>
2025-06-19 17:39:56 -0700

    just trudging along not getting the Laplacian of the Dawson

--- result.py
+++ result.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.signal import convolve,hilbert
 from scipy.special import dawsn as D
 from scipy.special import wofz as w
-from scipy.special import erfc
+from scipy.special import erf
 import matplotlib.pyplot as plt
 
 def dwdq(q):
@@ -11,6 +11,12 @@ def dwdq(q):
 
 def s(t,E0,sigma_t):
 	return -E0*t*np.exp(-0.5*t*t/sigma_t/sigma_t)
+
+def r(t,R0,f0,gamma):
+	if(t>=0):
+		return R0*np.cos(2*np.pi*f0*t)*np.exp(-2*np.pi*gamma*t)
+	else:
+		return 0
 
 def math_conv(t,E0,R0,sigma_t,f0,gamma):
 	J = complex(0,1)
@@ -34,21 +40,21 @@ def math_env(t,E0,R0,sigma_t,f0,gamma):
 	z = (2*np.pi*J*f0-2*np.pi*gamma)*np.sqrt(2)*sigma_t
 	q = -J*(x+z/2)
 	first_term = -np.sqrt(np.pi)*units*(x*np.exp(-x*x)*w(q)+0.5*J*np.exp(-x*x)*dwdq(q))
-	second_term = 2/np.sqrt(np.pi)*units
-	result = first_term+J*0
+	second_term = 0
+	result = first_term+J*second_term
 	if(np.isinf(result) or np.isnan(result)):
 		return 0
 	else:
 		return 0.5*np.abs(result)
 
-f0 = 0.2 #GHz
-gamma = 0.15 #GHz
-sigma_t = 2 #ns
+f0 = 0.33 #GHz
+gamma = 0.04 #GHz
+sigma_t = 1 #ns
 R0 = 1
 E0 = 1
 dt = 0.01 #ns
-T_min = -10 #ns
-T_max = 40 #ns
+T_min = -50 #ns
+T_max = 50 #ns
 t = np.arange(T_min,T_max,dt)
 n = len(t)
 
@@ -56,18 +62,20 @@ J = complex(0,1)
 x = t/(sigma_t*np.sqrt(2))
 z = (2*np.pi*J*f0-2*np.pi*gamma)*np.sqrt(2)*sigma_t
 q = -J*(x+z/2)
+a=4
 
 result1 = np.zeros(t.shape,dtype=complex)
 result2 = np.zeros(t.shape,dtype=complex)
 for i in range(n):
-	result1[i] = math_conv(t[i],E0,R0,sigma_t,f0,gamma)
-	result2[i] = math_env(t[i],E0,R0,sigma_t,f0,gamma)
-graph1 = np.real(result1)*1000
-graph2 = np.abs(hilbert(np.real(result1)))*1000
-graph3 = np.real(result2)*1000
-plt.plot(t,graph1,'-',label="mathematical convolution")
-plt.plot(t,graph2,'-',label="envelope of mathematical convolution")
-plt.plot(t,graph3,'-',label="mathematical envelope")
+	u = t[i]
+	result1[i] = math_conv(u,E0,R0,sigma_t,f0,2*np.pi*gamma)
+	u = u+0.5
+	u*=0.2
+	result2[i] = 2*np.exp(-0.5*u*u)*(0.5*(1+erf(a*u/np.sqrt(2))))/1.6
+graph1 = np.abs(hilbert(np.real(result1)))*1000
+graph2 = np.real(result2)*1000
+plt.plot(t,graph1,'-',label="env of math conv")
+plt.plot(t,graph2,'-',label="math env")
 plt.xlabel("Time [ns]")
 plt.ylabel("Amplitude [mV]")
 plt.xticks()
