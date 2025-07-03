2e507a29b14fb6d6da5eb14ff3d81ab75a6692b1
Jordan C hanson <918particle@gmail.com>
2025-07-03 11:06:11 -0700

    Add files via upload

--- result.py
+++ result.py
@@ -2,9 +2,6 @@ import numpy as np
 from scipy.signal import convolve,hilbert
 from scipy.special import dawsn as D
 from scipy.special import wofz as w
-from scipy.special import factorial2
-from mpmath import gamma as gammaFunction
-from mpmath import gammainc
 import matplotlib.pyplot as plt
 from scipy.integrate import quad
 
@@ -25,21 +22,20 @@ def r(t,R0,f0,gamma):
 		return R0*np.cos(2*np.pi*f0*t)*np.exp(-2*np.pi*gamma*t)
 	else:
 		return 0
-
+        
 def math_conv(t,E0,R0,sigma_t,f0,gamma):
-	J = complex(0,1)
-	sigma_f = 1.0/(2*np.pi*sigma_t)
-	p = (t/sigma_t)*(sigma_f/f0) - gamma/(2*np.pi*f0)
-	q = 0.5*(sigma_f/f0)*(sigma_f/f0)
-	z = complex(1.0/(2.0*np.sqrt(q)),-p/(2.0*np.sqrt(q)))
-	U = np.sqrt(np.pi/(4.0*q))*np.real(w(z))
-	dUdp = np.sqrt(np.pi)/(4*q)*(2/np.sqrt(np.pi) + 2*np.real(J*z*w(z)))
-	first_term = np.sqrt(np.pi/2)*sigma_t*R0*s(t,E0,sigma_t)*U
-	second_term = np.sqrt(np.pi/2)*E0*R0*sigma_t*sigma_t*(sigma_f/f0)*np.exp(-0.5*(t/sigma_t)*(t/sigma_t))*dUdp
-	if(np.isinf(first_term) or np.isinf(second_term) or np.isnan(first_term) or np.isnan(second_term)):
-		return 0.0
-	else:
-		return first_term+second_term
+    J = complex(0,1)
+    units = R0*E0*sigma_t*sigma_t
+    x = t/(sigma_t*np.sqrt(2))
+    z = (2*np.pi*J*f0-2*np.pi*gamma)*np.sqrt(2)*sigma_t
+    q = -J*(x+0.5*z)
+    Re_wq = np.real(w(q))
+    Re_dwdx = np.real(-J*dwdq(q))
+    result = -np.sqrt(np.pi)*units*(x*np.exp(-x*x)*Re_wq-0.5*np.exp(-x*x)*Re_dwdx)
+    if(np.isinf(result) or np.isnan(result)):
+        return 0.0
+    else:
+        return result
 
 def math_env(t,E0,R0,sigma_t,f0,gamma):
 	units = R0*E0*sigma_t*sigma_t
@@ -56,13 +52,13 @@ def math_env(t,E0,R0,sigma_t,f0,gamma):
 		result = first_term+J*second_term
 	return 0.5*np.abs(result)
 
-f0 = 0.25 #GHz
+f0 = 0.3 #GHz
 gamma = 0.05 #GHz
 sigma_t = 0.5 #ns
 R0 = 1
 E0 = 1
 dt = 0.01 #ns
-T_min = -20 #ns
+T_min = -10 #ns
 T_max = 100 #ns
 t = np.arange(T_min,T_max,dt)
 n = len(t)
@@ -70,20 +66,14 @@ n = len(t)
 result1 = np.zeros(t.shape,dtype=complex)
 result2 = np.zeros(t.shape,dtype=complex)
 for i in range(n):
-	u = t[i]
-	result1[i] = math_conv(u,E0,R0,sigma_t,f0,2*np.pi*gamma)
-	result2[i] = math_env(u,E0,R0,sigma_t,f0,gamma)
+    u = t[i]
+    result1[i] = math_conv(u,E0,R0,sigma_t,f0,gamma)
+    result2[i] = math_env(u,E0,R0,sigma_t,f0,gamma)
 graph1 = np.abs(hilbert(np.real(result1)))*1000
 graph2 = np.real(result2)*1000
 graph3 = np.real(result1)*1000
 
-with open("results_June27th.dat","w") as file:
-	for i in range(n):
-		writeString = str(t[i])+" "+str(graph3[i])+" "+str(graph1[i])+" "+str(graph2[i])+"\n"
-		file.write(writeString)
-file.close()
-
-'''plt.plot(t,graph1,'-',label="env of math conv")
+plt.plot(t,graph1,'-',label="env of math conv")
 plt.plot(t,graph2,'-',label="math env")
 plt.plot(t,graph3,'-',label="math conv")
 plt.xlabel("Time [ns]")
@@ -91,4 +81,4 @@ plt.ylabel("Amplitude [mV]")
 plt.xticks()
 plt.yticks()
 plt.legend()
-plt.show()'''
+plt.show()
