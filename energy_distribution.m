d4dc235788f356c6ffc9b8976d3b5559b6db1a1c
Jordan C hanson <918particle@gmail.com>
2025-09-18 16:15:59 -0700

    grinding towards the final results in Table III

--- energy_distribution.m
+++ energy_distribution.m
@@ -7,40 +7,8 @@ linewidth = 1.5;
 results = load('all_results.dat');
 c = 0.3/1.78;
 sinC = sin(55.8*pi/180.0);
-dTheta = 4.3*pi/180.0;
-jj = find(results(:,6)<3);
-sigma = results(jj,6);
-cem = 0.825;
-log10E = (c*sigma./cem/dTheta/sinC).^2/log(10.0)+8.0;
-mean_log10E = mean(log10E);
-std_log10E = std(log10E);
-x_bin_delta = 1.0;
-x_bin_min = 10.0;
-x_bin_max = 40.0;
-xbins = x_bin_min:x_bin_delta:x_bin_max;
-[y,x] = hist(log10E,xbins,1);
-
-%Prediction from NuRadioMC, 100 PeV
-m = 17.0;
-s = 2;
-shade_delta = x_bin_delta/100.0;
-shade_bins = x_bin_min:shade_delta:x_bin_max;
-region = zeros(size(shade_bins));
-region(find(and(shade_bins>=(m-s),shade_bins<=(m+s)))) = 1;
-
-figure(1);
-hold on;
-bar(x,y,'facecolor','white','edgecolor','black','linewidth',linewidth);
-axis([10 40 0.0 1]);
-h = area(shade_bins,region);
-set(h,'facecolor',[0.5 0.5 0.5]);
-set(h,'edgecolor',[0.5 0.5 0.5]);
-set(h,'facealpha',0.2);
-set(h,'edgealpha',0.9);
-plot([m m],[0 1],'-','color','black','linewidth',linewidth);
-labelString = ['mean: ',num2str(mean_log10E,3),' std: ',num2str(std_log10E,3)];
-text(16.0,0.26,labelString,'fontname','courier','fontsize',fontsize)
-set(gca(),'fontname','courier','fontsize',fontsize,'box','on');
-xlabel('log_{10} E_{C}','fontname','courier','fontsize',fontsize);
-ylabel('Probability (Normalized)','fontname','courier','fontsize',fontsize);
-print('Aug19_plot1.pdf','-dpdf');
+jj = find(and(abs(results(:,9)*180/pi-55.8)>0,results(:,6)<3.0));
+sigma = mean(results(jj,6));
+dTheta = std(results(jj,9)-55.8*pi/180.0);
+cem = 0.8;
+log10E = (c*sigma./cem./dTheta/sinC).^2/log(10.0)+8.0
--- viewing_angle.m
+++ viewing_angle.m
@@ -7,7 +7,7 @@ markersize = 6;
 linewidth = 1.5;
 results = load('all_results.dat');
 theta = results(:,9)*180.0/pi;
-x_bin_delta = 2;
+x_bin_delta = 2.5;
 x_bin_min = 0.0;
 x_bin_max = 90.0;
 xbins = x_bin_min:x_bin_delta:x_bin_max;
@@ -21,14 +21,13 @@ cAngle = acos(1.0/1.78)*180.0/pi;
 
 figure(1);
 hold on;
-semilogy(x1,y1,'o','color','black','markersize',markersize);
-semilogy(xfit,yfit,'-','color','black','linewidth',linewidth);
-axis([0 90 1e-4 10]);
-plot([cAngle cAngle],[1e-4 10],'--','color','#444444','linewidth',linewidth)
-legendString = ['\mu: ' num2str(p1(1),3) ' \sigma: ' num2str(p1(2),3)];
-h = legend('NuRadioMC output',legendString,'Cherenkov Angle','location','northwest');
+plot(x1-55.8,y1,'o','color','black','markersize',markersize);
+plot(xfit-55.8,yfit,'-','color','black','linewidth',linewidth);
+axis([-30 30 1e-4 0.75]);
+legendString = ['\mu: ' num2str(p1(1)-55.8,3) ' \sigma: ' num2str(p1(2),3)];
+h = legend('NuRadioMC output',legendString,'location','northwest');
 set(h,'fontname','courier','fontsize',fontsize)
 set(gca(),'fontname','courier','fontsize',fontsize,'box','on');
-xlabel('Viewing Angle (deg)','fontname','courier','fontsize',fontsize);
+xlabel('\Delta\theta (deg)','fontname','courier','fontsize',fontsize);
 ylabel('Probability (Normalized)','fontname','courier','fontsize',fontsize);
 print('Aug18_plot1.pdf','-dpdf');
