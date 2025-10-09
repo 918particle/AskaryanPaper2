40a9a4e30d8f1b27761a74de73d38408c7a2d2f5
Jordan C hanson <918particle@gmail.com>
2025-10-09 16:44:33 -0700

    added code to paper in appendix

--- energy_distribution.m
+++ energy_distribution.m
@@ -4,8 +4,8 @@ home;
 results = load('all_results_100PeV.dat');
 c = 0.3/1.78;
 sinC = sin(55.8*pi/180.0);
-jj = find(and(abs(results(:,9)*180/pi-55.8)>0,results(:,6)<3.0));
+jj = find(and(abs(results(:,9)*180/pi-55.8)>0.0,results(:,6)<3.0));
 sigma = mean(results(jj,6));
 dTheta = std(results(jj,9)-55.8*pi/180.0);
-cem = 0.86;
+cem = 0.85;
 log10E = (c*sigma./cem./dTheta/sinC).^2/log(10.0)+8.0
