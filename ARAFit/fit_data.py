97d591fbfdd06a7ebb15b72986d799f71556b018
Jordan C hanson <918particle@gmail.com>
2025-11-06 10:22:44 -0800

    added ARA data to fit, plus code

--- ARAFit/fit_data.py
+++ ARAFit/fit_data.py
@@ -7,7 +7,7 @@ from scipy.signal import convolve,hilbert
 from scipy.special import dawsn as D
 from scipy.special import wofz as w
 from scipy.integrate import quad
-from scipy.signal import correlate
+from scipy.signal import correlate,correlation_lags
 
 def shifted_laplace_transform(f,s,t0,t_max=1000):
     real_integral = quad(lambda t: np.exp(-s.real*t)*np.cos(s.imag*t)*f(t-t0),0,t_max)[0]
@@ -36,6 +36,34 @@ def math_conv(t,E0,R0,sigma_t,f0,gamma):
         return 0.0
     else:
         return result
+def math_env(t,E0,R0,sigma_t,f0,gamma):
+	units = R0*E0*sigma_t*sigma_t
+	J = complex(0,1)
+	x = t/(sigma_t*np.sqrt(2))
+	z = (2*np.pi*J*f0-2*np.pi*gamma)*np.sqrt(2)*sigma_t
+	k=-z
+	q = -J*(x+z/2)
+	first_term = -np.sqrt(np.pi)*units*(x*np.exp(-x*x)*w(q)+0.5*J*np.exp(-x*x)*dwdq(q))
+	second_term = 2/np.sqrt(np.pi)*units*(D(x) + k*shifted_laplace_transform(D,k,x))
+	if(np.isinf(first_term) or np.isinf(second_term) or np.isnan(first_term) or np.isnan(second_term)):
+		return 0.0
+	else:
+		result = first_term+J*second_term
+	return 0.5*np.abs(result)
+def get_csw(trace_set,index):
+    ref = trace_set[index]
+    n = len(ref)
+    sum_trace = np.copy(ref)
+    lag_array = correlation_lags(n,n,mode='full')
+    for i, trace in enumerate(trace_set):
+        corr = correlate(ref,trace_set[i],mode='full')/n
+        best_lag = lag_array[np.argmax(corr)]
+        aligned_trace = np.roll(trace,best_lag)
+        sum_trace += aligned_trace
+    sum_trace -= np.mean(sum_trace)
+    norm = np.sqrt(np.inner(sum_trace,sum_trace))
+    sum_trace/=norm
+    return sum_trace
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", help="Path to the file to open")
@@ -43,22 +71,53 @@ args = parser.parse_args()
 
 #Data: 16 channels, 512 samples per channel, 1.5 GHz sampling rate
 fs = 1.5
-data = np.zeros((16,512))
 i=0
-times = np.arange(0,512)/fs
+n = 512
+data = np.zeros((7,n))
+times = np.arange(0,n)/fs
+good_channels = [0,1,2,3,4,6,7]
 
 with open(args.filename, newline='') as csvfile:
 	reader = csv.reader(csvfile, delimiter=',')
 	for row in reader:
-		data[i] = list(map(float,row))
-		i+=1
+		if(i in good_channels):
+			data[i] = list(map(float,row))
+		if(i==6):
+			break
+		else:
+			i+=1
 
-model = np.zeros(512)
-for i in range(512):
-	model[i] = math_conv(times[i]-125,1,1,0.45,0.180,0.0125)
+csw = get_csw(data,6)
+model = np.zeros(n)
+env_model = np.zeros(n)
+rho_max = 0
+f0s = [0.170,0.175,0.180,0.185,0.190]
+gammas = [0.011,0.012,0.013,0.014,0.015]
+sigmas = [0.425,0.45,0.475,0.5,0.525,0.55]
+best_sigma = 0
+best_gamma = 0
+best_f0 = 0
+for f0 in f0s:
+	for gamma in gammas:
+		for sigma in sigmas:
+			for i in range(n):
+				model[i] = math_conv(times[i]-154,1,1,sigma,f0,gamma)
+			model = model/np.sqrt(np.inner(model,model))
+			rho = np.max(np.abs(correlate(model,csw)))
+			if(rho>rho_max):
+				best_f0=f0
+				best_gamma=gamma
+				best_sigma=sigma
+				rho_max = rho
+print("Best fit values")
+print(best_sigma,best_f0,best_gamma,rho_max)
+print("Data:")
+for i in range(n):
+	model[i] = math_conv(times[i]-154,1,1,best_sigma,best_f0,best_gamma)
+	env_model[i] = math_env(times[i]-154,1,1,best_sigma,best_f0,best_gamma)
 model = model/np.sqrt(np.inner(model,model))
-data_n = data[3]/np.sqrt(np.inner(data[3],data[3]))
-print(np.max(np.abs(correlate(model,data_n))))
-plt.plot(times,model)
-plt.plot(times,data_n)
-plt.show()
+env_model = env_model/np.sqrt(np.inner(env_model,env_model))
+csw_env = np.abs(hilbert(np.real(csw)))
+csw_env = csw_env/np.sqrt(np.inner(csw_env,csw_env))
+for i in range(n):
+	print(times[i],csw[i],model[i],env_model[i],csw_env)
---
+++ ARAFit/plot_results.plt
@@ -0,0 +1,39 @@
+set terminal pdf enhanced
+set output "November2nd_plot1.pdf"
+unset grid
+set xrange [120:340]
+set yrange [-0.5:0.5]
+set xlabel "Time [ns]" font "Courier,18" offset 0,-0.5
+set ylabel "Amplitude [norm.]" font "Courier,18" offset -2,0
+set xtics 125,25,350 font "Courier,18"
+set ytics font "Courier,18"
+set lmargin 10
+set rmargin 5
+set tmargin 1
+set bmargin 4
+set key right top box on font "Courier,14" width 1.1 spacing 1.1 at 325,0.45
+set style line 1 lw 3.0 lc rgb "#AAAAAA"
+set style line 2 lw 1.0 lc rgb "#000000"
+set label 1 "{/Symbol s}_t: 0.425 ns" at 230,0.3 font "Courier,12"
+set label 2 "f_0: 0.19 GHz" at 230,0.25 font "Courier,12"
+set label 3 "{/Symbol g}: 0.014 GHz" at 230,0.2 font "Courier,12"
+set label 4 "{/Symbol r}: 0.853" at 230,0.15 font "Courier,12"
+set multiplot
+set origin 0,0
+set size 1,1
+plot "result_Nov2.dat" using 1:2 w l ls 1 title "CSW, ARA CR event", \
+"result_Nov2.dat" using 1:3 w l ls 2 title "Model"
+set origin 0.4,0.1
+set size 0.55,0.45
+set xrange [0:350]
+set yrange [-0.5:0.5]
+unset key
+unset xlabel
+unset ylabel
+unset label 1
+unset label 2
+unset label 3
+unset label 4
+set xtics 0,100,350  font "Courier,14"
+set ytics font "Courier,14"
+plot "result_Nov2.dat" using 1:2 w l ls 1, "result_Nov2.dat" using 1:3 w l ls 2
